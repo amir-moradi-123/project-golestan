80627d55d7527dd8035066163dbb7f18e6c4c333
amir <amitamitamit1383@gmail.com>
2024-06-13 04:19:54 -0700

    compelete projekt

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.html"],
+  theme: {
+    screens:{
+      "mobile":"0",
+      "desctop":"1000px"
+    },
+    extend: {
+fontFamily:{
+  'fontsans':"fontgol",
+
+},
+    },
+  },
+  plugins: [],
+}
+
