11f253749a3ea165911eec19f1f06aeff711e3b3
amir <amitamitamit1383@gmail.com>
2024-06-19 13:30:24 -0700

    compelete golestan

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./*.html"],
+  darkMode:"class",
   theme: {
     screens:{
       "mobile":"0",
-      "desctop":"1000px"
+      "desctop":"1000px",
     },
     extend: {
 fontFamily:{
